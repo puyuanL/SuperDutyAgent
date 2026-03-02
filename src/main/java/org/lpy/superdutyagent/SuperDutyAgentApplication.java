71014207f9df783a50244cb2692ba55d0a1a57ca
puyuan <puyuan.l@qq.com>
2026-03-02 15:48:40 +0800

    Init SuperDutyAgent Project.

---
+++ src/main/java/org/lpy/superdutyagent/SuperDutyAgentApplication.java
@@ -0,0 +1,11 @@
+package org.lpy.superdutyagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDutyAgentApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SuperDutyAgentApplication.class, args);
+	}
+}
